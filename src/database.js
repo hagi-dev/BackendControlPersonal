db745d344759d4f2166aa1dda0a2770b85cb1a08
Hagi <hagiraitorresmacedo@gmail.com>
2025-02-18 15:45:05 -0500

    update connect db

--- src/database.js
+++ src/database.js
@@ -1,50 +1,45 @@
 require('dotenv').config();
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const { promisify } = require('util');
-const database ={
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'control_personal',
-}
+const { URL } = require('url');
+
+// Parsear la cadena de conexión
+const connectionString = process.env.BD_URL_CONNECTION || '';
+const dbUrl = new URL(connectionString);
+
+const database = {
+  host: dbUrl.hostname,
+  user: dbUrl.username,
+  password: dbUrl.password,
+  database: dbUrl.pathname.substring(1),
+  port: dbUrl.port,
+  connectTimeout: process.env.DB_CONNECTION_TIMEOUT || 10000, // Aumentar el tiempo de espera de la conexión
+};
+
 const pool = mysql.createPool(database);
-pool.getConnection((err, connection) => {
-    if (err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.')
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.')
-        }
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.')
-        }
+
+pool.getConnection()
+  .then(connection => {
+    console.log('DB is connected');
+    connection.release();
+  })
+  .catch(err => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.');
     }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.');
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.');
+    }
+    if (err.code === 'ETIMEDOUT') {
+      console.error('Database connection timed out.');
+    }
+    console.error('Error connecting to the database:', err);
+  });
 
-    if (connection) connection.release()
-    console.log('DB is connected 1');
-    return
-});
-
-//promisefy pool query
+// Promisify pool query
 pool.query = promisify(pool.query);
 
 module.exports = pool;
-
-
-// const mysqlConnection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'control_personal',
-//     multipleStatements: true
-// });
-
-// mysqlConnection.connect(function (err) {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }else {
-//         console.log('DB is connected');
-//     }
-// });
